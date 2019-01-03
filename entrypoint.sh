8f849cd4ebe631550a1d6a6bc3a380cc5376ec96
Niels Lemmens <draso.odin@gmail.com>
2019-01-03 10:48:03 +0100

    Check for Pipfile and requirements.txt

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,15 @@
 
 set -eax
 
+if [ -f Pipfile ]
+then
+    pip install -U pipenv
+    pipenv install -d --system
+elif [ -f requirements.txt ]
+then
+    pip install -r requirements.txt
+fi
+
 isort --check-only --diff --quiet
 black --check --exclude node_modules .
 flake8
